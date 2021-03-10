cb5e13a69bf52edde5eba2040cf72f2d02730d33
Vlad Shutikov <vlad5565@mail.ru>
2021-03-10 15:41:08 +0300

    init: start basic-js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members == null) {
+    return false;
+  }
+  str = '';
+  members.forEach(element => {
+    if (typeof element == 'string') {
+      str = str + element.trim()[0].toUpperCase();
+    }
+  });
+  return str.split('').sort((a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+  }).join('');
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (date == null) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date)) {
+    throw new Error();
+  }
+  let season = '0';
+  const MONTH = date.getMonth();
+  switch (MONTH) {
+    case 11:
+    case 0:
+    case 1:
+      season = 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      season = 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      season = 'summer';
+      break;
+    case 8:
+    case 9:
+    case 10:
+      season = 'fall';
+      break;
+
+  }
+
+  return season;
+
 };
