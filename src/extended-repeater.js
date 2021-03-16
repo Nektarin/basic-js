aa31a1342f7ca28bfcf7e988ed388bd739c3223c
Vlad <vshutikov10@gmail.com>
2021-03-16 22:46:33 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let string = str;
+  if (string === null) {
+    string = "null";
+  }
+  let separator = "+";
+  let someAddition = "";
+  let additionCopy = options.addition;
+  let additionSeparator = "|";
+
+  if (options.separator != undefined) {
+    separator = options.separator;
+  }
+
+  if (additionCopy !== undefined) {
+    if (additionCopy === null) {
+      additionCopy = "null";
+    }
+
+    if (options.additionSeparator != undefined) {
+      additionSeparator = options.additionSeparator;
+    }
+
+    if (
+      options.additionRepeatTimes < 2 ||
+      options.additionRepeatTimes == undefined
+    ) {
+      someAddition += additionCopy;
+    } else {
+      someAddition = additionCopy + additionSeparator;
+
+      for (let index = 0; index < options.additionRepeatTimes - 1; index++) {
+        if (index == options.additionRepeatTimes - 2) {
+          someAddition += additionCopy;
+        } else {
+          someAddition += additionCopy + additionSeparator;
+        }
+      }
+    }
+  }
+
+  for (let index = 0; index < options.repeatTimes - 1; index++) {
+    string += someAddition + separator + str;
+  }
+
+  if (options.addition !== undefined) {
+    return string + someAddition;
+  } else {
+    return string;
+  }
 };
-  
