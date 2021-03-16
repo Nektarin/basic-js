f86306ef5991ed07956ebd9227b046c03e470e8f
Vlad Shutikov <vlad5565@mail.ru>
2021-03-16 12:45:26 +0300

    feat implement two functions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let maxdepth = 0;
+    if (Array.isArray(arr)) {
+      for (let key in arr) {
+        let dpth = this.calculateDepth(arr[key]);
+        if (dpth > maxdepth)
+          maxdepth = dpth;
+      }
+      return ++maxdepth;
+    }
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -8,26 +8,43 @@ module.exports = function transform(arr) {
 
   for (let index = 0; index < arr2.length; index++) {
 
-    if (Array.isArray(arr2[index])) {
-      arr2[index] = transform(arr2[index]).slice();
-    }
+    // if (Array.isArray(arr2[index])) {
+    //     arr2[index] = transform(arr2[index]).slice();
+    // }
 
     if (index == 0 && (arr2[index] == '--discard-prev' || arr2[index] == '--double-prev')) {
       arr2.splice(index, 1);
+      arr2 = transform(arr2).slice();
     } else if (index == arr2.length - 1 && (arr2[index] == '--discard-next' || arr2[index] == '--double-next')) {
       arr2.splice(index, 1);
+      arr2 = transform(arr2).slice();
     } else if (arr2[index] == '--discard-next') {
       if (arr2[index + 2] == '--double-prev' || arr2[index + 2] == '--discard-prev') {
         arr2.splice(index, 3);
+        arr2 = transform(arr2).slice();
+      } else if (arr2[index + 1] == '--double-next' || arr2[index + 1] == '--double-prev' ||
+        arr2[index + 1] == '--discard-prev' || arr2[index + 1] == '--discard-next') {
+        arr2.splice(index, 1);
+        arr2 = transform(arr2).slice();
       } else {
         arr2.splice(index, 2);
+        arr2 = transform(arr2).slice();
       }
     } else if (arr2[index] == '--discard-prev') {
       arr2.splice(index - 1, 2);
+      arr2 = transform(arr2).slice();
     } else if (arr2[index] == '--double-next') {
-      arr2.splice(index, 1, arr2[index + 1]);
+      if (arr2[index + 1] == '--double-next' || arr2[index + 1] == '--double-prev' ||
+        arr2[index + 1] == '--discard-prev' || arr2[index + 1] == '--discard-next') {
+        arr2.splice(index, 1);
+        arr2 = transform(arr2).slice();
+      } else {
+        arr2.splice(index, 1, arr2[index + 1]);
+        arr2 = transform(arr2).slice();
+      }
     } else if (arr2[index] == '--double-prev') {
       arr2.splice(index, 1, arr2[index - 1]);
+      arr2 = transform(arr2).slice();
     }
   }
   return arr2;
