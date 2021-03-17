8f282a59625db65a21eea0d538f4fd6b59092613
Vlad Shutikov <vlad5565@mail.ru>
2021-03-17 09:10:35 +0300

    implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,76 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(version) {
+    this.version = true;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+    if (version == false) {
+      this.version = false;
+    }
+  }
+
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error();
+    }
+
+    let index2 = 0;
+    let text = message.toUpperCase();
+    let fullKey = key.toUpperCase();
+    let encrypted = '';
+
+    while (fullKey.length < text.length) {
+      fullKey += key.toUpperCase();
+    }
+
+    for (let index = 0; index < text.length; index++) {
+      if (this.alphabet.includes(text[index])) {
+        encrypted += this.alphabet[((this.alphabet.indexOf(text[index]) +
+          this.alphabet.indexOf(fullKey[index2])) % 26)];
+        index2++;
+      } else {
+        encrypted += text[index];
+      }
+
+    }
+    if (this.version == true) {
+      return encrypted;
+    } else {
+      return encrypted.split('').reverse().join('');
+    }
+
+  }
+
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error();
+    }
+
+    let index2 = 0;
+    let text = message.toUpperCase();
+    let fullKey = key.toUpperCase();
+    let encrypted = '';
+
+    while (fullKey.length < text.length) {
+      fullKey += key.toUpperCase();
+    }
+
+    for (let index = 0; index < text.length; index++) {
+      if (this.alphabet.includes(text[index])) {
+        encrypted += this.alphabet[((this.alphabet.indexOf(text[index]) -
+          this.alphabet.indexOf(fullKey[index2]) + 26) % 26)];
+        index2++;
+      } else {
+        encrypted += text[index];
+      }
+
+    }
+    if (this.version == true) {
+      return encrypted;
+    } else {
+      return encrypted.split('').reverse().join('');
+    }
   }
 }
 
